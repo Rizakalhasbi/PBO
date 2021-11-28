7e4b69b2ce8d9e9933e82628cecb45103f01fc65
Rizak Al Hasbi Anwar <71827092+Rizakalhasbi@users.noreply.github.com>
2021-11-28 09:32:25 +0700

    Add files via upload

---
+++ Chapter 4/ProjectPengubinan/src/projectpengubinan/ProjectPengubinan.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectpengubinan;
+
+/**
+ *
+ * @author ACER
+ */
+public class ProjectPengubinan {
+    int panjang;
+    int lebar;
+
+    int hitungLuas(){
+        int luas = this.panjang * this.lebar;
+        return luas;
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ Chapter 4/ProjectPengubinan/src/projectpengubinan/Ruang.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectpengubinan;
+
+/**
+ *
+ * @author ACER
+ */
+public class Ruang {
+    int panjang;
+    int lebar;
+    
+    int hitungLuas(){
+    PersegiPanjang p = new PersegiPanjang();
+    }
+    
+    p.panjang = this.panjang;
+    p.lebar = this.lebar;
+    
+    return p.hitungLuas();
+}
