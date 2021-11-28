af75d0077e6f5d96877eaab62bdd8dd41275ceb1
Rizak Al Hasbi Anwar <71827092+Rizakalhasbi@users.noreply.github.com>
2021-11-28 09:36:24 +0700

    Add files via upload

---
+++ Chapter 5/ProjectAdvancedOperasi/src/projectadvancedoperasi/Operasi.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectadvancedoperasi;
+
+/**
+ *
+ * @author ACER
+ */
+public class Operasi {
+    int jumlahkan(int a, int b){
+    return (a+b);
+    }
+    
+    int jumlahkan(int a, int b, int c){
+    return (a+b+c);
+    } 
+    
+    int jumlahkan(double a, double b, double c){
+    return (int) (a+b+c);
+    }
+}
