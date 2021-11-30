11c90fde6207f19ad5cdf5e79ca09459ee6f65c0
Rizak Al Hasbi Anwar <71827092+Rizakalhasbi@users.noreply.github.com>
2021-11-30 18:42:12 +0700

    Add files via upload

---
+++ Chapter 11/ProjectMinMax/src/projectminmax/ProjectMinMax.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectminmax;
+
+/**
+ *
+ * @author ACER
+ */
+public class ProjectMinMax {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    ArrayProcessing R = new ArrayProcessing();
+        R.input();
+        R.output();
+    }
+    
+}
