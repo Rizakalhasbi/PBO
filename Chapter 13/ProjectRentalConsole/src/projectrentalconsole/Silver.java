5f13a63e55a80e857636bbda04a6c371eb887ab7
Rizak Al Hasbi Anwar <71827092+Rizakalhasbi@users.noreply.github.com>
2021-11-30 19:00:29 +0700

    Add files via upload

---
+++ Chapter 13/ProjectRentalConsole/src/projectrentalconsole/Silver.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectrentalconsole;
+
+/**
+ *
+ * @author ACER
+ */
+public class Silver {
+    protected int rentCost, point, disc, tempCost, discountAmount, totalCost;
+    
+    Silver() {
+        this.rentCost = 25000;
+        this.point = 1;
+        this.disc = 1;
+    }
+    
+    protected int costAmount(int rentalDuration) {
+        
+        tempCost = rentalDuration * rentCost;
+        discountAmount = (tempCost * disc) / 100;
+        totalCost = tempCost - discountAmount;
+        
+        return totalCost;
+    }
+    
+    protected int getPoint(int rentalDuration) {
+        
+        int totalPoint = rentalDuration * point;
+        return totalPoint;
+    }
+}
