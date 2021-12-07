93c3c9d9c9ee4aa25431ccfec3517d4fa5ad8d36
Rizak Al Hasbi Anwar <71827092+Rizakalhasbi@users.noreply.github.com>
2021-12-07 19:08:25 +0700

    Add files via upload

---
+++ Chapter 15/ProjectBangun/src/bangundatar/Lingkaran.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bangundatar;
+
+/**
+ *
+ * @author ACER
+ */
+public class Lingkaran extends BangunDatar {
+    // atribut jari-jari
+    public double ...;
+     
+    // method untuk hitung luas lingkaran
+    public double hitungLuas(){
+        ...
+    }
+    
+    // method untuk hitung keliling
+    public double hitungKeliling(){
+ 	  ...
+    }
+}
